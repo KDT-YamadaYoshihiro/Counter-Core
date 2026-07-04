3b016b2e7473d640f1431ae921c1cbc56409172d
4C-07\koyo <a>
2026-07-04 19:09:30 +0900

    初期化処理

--- Counter_Core/Source/Counter_Core/Counter_Core.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Counter_Core.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Counter_Core, "Counter_Core" );
-
-DEFINE_LOG_CATEGORY(LogCounter_Core)
